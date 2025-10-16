e84eef5069739a6e7694718e7e58081506e5554c
zihasoo <dlquddbs1234@gmail.com>
2025-10-16 23:37:08 +0900

    과제 기능 5개 추가 완료 - 더블 점프 - 낙사 - 스파이크 및 스파이크 헤드 장애물 - 회복 과일 - 점수 시스템

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,15 +8,17 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public GameObject hitPrefab;
-    public GameObject gameOver;
-    public GameObject gameClear;
-    public Slider slider;
-    public Text HPText;
+    public UIManager uiManager;
+
     public int maxHP;
     public int maxJumpCount = 2;
-
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
+    public float gameOverHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
@@ -24,12 +27,14 @@ public class Player : MonoBehaviour
     private int jumpCount;
     private int HP;
     private bool isFalling;
+    private Animator animator;
+    private int itemCount = 0;
+    private int hitCount = 0;
 
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.1f;
-    public LayerMask groundLayer;
-
-    Animator animator;
+    private List<string> animeParameters = new()
+    {
+        "IsIDLE", "IsRun", "IsJump", "IsFalling"
+    };
 
     private void Start()
     {
@@ -42,13 +47,21 @@ public class Player : MonoBehaviour
         isGrounded = true;
         HP = maxHP;
         jumpCount = maxJumpCount;
-
-        HPText.text = $"HP: {maxHP}";
+        uiManager.init(HP);
     }
 
     void Update()
     {
         if (isOver) return;
+
+        if (transform.position.y < gameOverHeight)
+        {
+            PlayHitEffect();
+            isOver = true;
+            gameObject.SetActive(false);
+            uiManager.GameOver();
+        }
+
         float moveInput = 0f;
         bool moveH = false;
 
@@ -68,50 +81,50 @@ public class Player : MonoBehaviour
         float vx = moveInput * moveSpeed;
         float vy = rb.velocityY;
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer) && rb.velocityY == 0;
-        if (isFalling && isGrounded) //처음으로 땅을 만났을 때
+        bool groundTest = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer) && rb.velocityY <= 1e-4;
+        if (!isGrounded && groundTest) //처음으로 땅을 만났을 때
         {
+            isGrounded = true;
             jumpCount = maxJumpCount;
             isFalling = false;
             animator.SetBool("IsFalling", false);
+            animator.SetBool("IsJump", false);
+        }
+        if (!groundTest)
+        {
+            isGrounded = false;
         }
 
+        bool hasJumped = false;
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
         {
+            hasJumped = true;
             jumpCount--;
             vy = jumpForce;
-            if (isGrounded)
-            {
-                animator.SetTrigger("IsJump");
-            }
-            else
+            SetParameterOnlyTrue("IsJump");
+            if (!isGrounded)
             {
                 isFalling = false;
-                animator.SetBool("IsFalling", false);
                 animator.SetTrigger("IsDoubleJump");
             }
         }
 
         if (!isFalling && !isGrounded && vy < 0) { //처음으로 떨어지기 시작할 때
             isFalling = true;
-            animator.SetBool("IsRun", false);
-            animator.SetBool("IsIDLE", false);
-            animator.SetBool("IsFalling", true);
+            SetParameterOnlyTrue("IsFalling");
         }
 
         rb.velocity = new Vector2(vx, vy);
 
-        if (!isGrounded) return;
+        if (!isGrounded || hasJumped) return;
 
         if (moveH)
         {
-            animator.SetBool("IsRun", true);
-            animator.SetBool("IsIDLE", false);
+            SetParameterOnlyTrue("IsRun");
         }
         else
         {
-            animator.SetBool("IsRun", false);
-            animator.SetBool("IsIDLE", true);
+            SetParameterOnlyTrue("IsIDLE");
         }
     }
 
@@ -120,17 +133,16 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             HP--;
-            var obj = Instantiate(hitPrefab, transform.position, Quaternion.identity);
-            Destroy(obj, 0.5f);
+            hitCount++;
+            PlayHitEffect();
             animator.SetTrigger("IsHit");
-            HPText.text = $"HP: {HP}";
-            slider.value = (float)HP / maxHP;
+            uiManager.SetHP(HP);
 
             if (HP <= 0)
             {
-                gameOver.SetActive(true);
                 isOver = true;
-                Destroy(gameObject);
+                gameObject.SetActive(false);
+                uiManager.GameOver();
             }
         }
         else if (collision.gameObject.CompareTag("Tram"))
@@ -139,15 +151,46 @@ public class Player : MonoBehaviour
             var tramAnimator = collision.gameObject.GetComponent<Animator>();
             tramAnimator.SetTrigger("IsJump");
             isFalling = false;
-            animator.SetBool("IsFalling", false);
-            animator.SetTrigger("IsJump");
+            SetParameterOnlyTrue("IsJump");
             jumpCount = maxJumpCount - 1;
+        }
+        else if (collision.gameObject.CompareTag("Item"))
+        {
+            int hpPlus = 1;
+            itemCount++;
+            if (collision.name == "Banana")
+            {
+                hpPlus = 2;
+            }
+            if (HP < maxHP)
+            {
+                HP = Mathf.Min(maxHP, HP + hpPlus);
+                uiManager.SetHP(HP);
+            }
+            var itemAnimator = collision.gameObject.GetComponent<Animator>();
+            itemAnimator.SetTrigger("Collected");
+            Destroy(collision.gameObject, 0.5f);
         }
         else if (collision.gameObject.CompareTag("End"))
         {
-            gameClear.SetActive(true);
+            uiManager.GameClear(itemCount, hitCount);
             isOver = true;
             rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void PlayHitEffect()
+    {
+        var obj = Instantiate(hitPrefab, transform.position, Quaternion.identity);
+        Destroy(obj, 0.5f);
+    }
+
+    private void SetParameterOnlyTrue(string param)
+    {
+        foreach (var p in animeParameters)
+        {
+            if (p == param) animator.SetBool(p, true);
+            else animator.SetBool(p, false);
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class UIManager : MonoBehaviour
 {
+    public Slider slider;
+    public Text HPText;
+    public Text timerText;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public GameObject gameClearScreen;
+
+    private int maxHP;
+    private float playTime;
+    private IEnumerator timeCoroutine;
+
+    public void init(int hp)
+    {
+        maxHP = hp;
+        slider.value = 1f;
+        slider.maxValue = 1f;
+        HPText.text = $"HP: {hp}";
+        timeCoroutine = Timer();
+        StartCoroutine(timeCoroutine);
+    }
+
+    public void SetHP(int hp)
+    {
+        HPText.text = $"HP: {hp}";
+        slider.value = (float)hp / maxHP;
+    }
+    
     public void RetryButton()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void GameClear(int itemCount, int hitCount)
+    {
+        int score = 0;
+        StopCoroutine(timeCoroutine);
+
+        //1초당 5점
+        //시간 최대점수는 900점
+        score = Math.Max((180 - (int)playTime) * 5 + itemCount * 150 - hitCount * 100, 0);
+        var text = $"플레이 시간: {playTime:F1}초\n시간: {(180 - (int)playTime) * 5}점\n" +
+            $"아이템 {itemCount}개: {itemCount * 150}점\n" +
+            $"충돌 {hitCount}회: {hitCount * -100}점\n" +
+            $"합계: {score}점";
+        scoreText.text = text;
+        gameClearScreen.SetActive(true);
+    }
+
+    private IEnumerator Timer()
+    {
+        playTime = 0f;
+        while (true)
+        {
+            timerText.text = $"Time: {playTime:F1}s";
+            playTime += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
